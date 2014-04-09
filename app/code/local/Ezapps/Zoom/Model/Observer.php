aab1fccf0142287c7d950721f208bf909fe32946
Mark Vermeltfoort <markvermeltfoort@acknowledge.nl>
2014-04-09 10:04:24 +0200

    Fixed page not found error for full page cache Fixed undefined stores error

--- app/code/local/Ezapps/Zoom/Model/Observer.php
+++ app/code/local/Ezapps/Zoom/Model/Observer.php
@@ -31,7 +31,7 @@ class Ezapps_Zoom_Model_Observer extends Mage_Core_Model_Abstract
 			Mage::getModel('ezzoom/page')->getCollection()
                                 ->addFieldToFilter('store_id', array('eq' => $store_id))
                                 ->addFieldToFilter('uri', array('eq' => "/" . $uri->getRequestPath()))->delete();
-		}				
+		}
 
                 $uris = Mage::getModel('core/url_rewrite')->getCollection()
                                 ->addFieldToFilter('store_id', array('eq' => $store_id))
@@ -64,7 +64,7 @@ class Ezapps_Zoom_Model_Observer extends Mage_Core_Model_Abstract
                 Mage::getModel('ezzoom/page')->getCollection()
                                 ->addFieldToFilter('store_id', array('eq' => $store_id))
                                 ->addFieldToFilter('uri', array('eq' => "/review/product/list/id/" . $product['entity_id']))->delete();
-		
+
 		Mage::getModel('ezzoom/page')->getCollection()
                                 ->addFieldToFilter('store_id', array('eq' => $store_id))
                                 ->addFieldToFilter('uri', array('like' => "/review/product/list/id/" . $product['entity_id'] . '/%'))->delete();
@@ -86,7 +86,7 @@ class Ezapps_Zoom_Model_Observer extends Mage_Core_Model_Abstract
 	else
         	foreach (Mage::getModel('core/store')->getCollection() as $store)
                 	$stores[] = $store->getId();
-        
+
         foreach ($stores as $store_id) {
 
                 $uris = Mage::getModel('core/url_rewrite')->getCollection()
@@ -98,10 +98,10 @@ class Ezapps_Zoom_Model_Observer extends Mage_Core_Model_Abstract
                                 ->addFieldToFilter('store_id', array('eq' => $store_id))
                                 ->addFieldToFilter('uri', array('eq' => "/" . $uri->getRequestPath()))->delete();
                 }
-	
+
 		$uris = Mage::getModel('core/url_rewrite')->getCollection()
                                 ->addFieldToFilter('store_id', array('eq' => $store_id))
-                                ->addFieldToFilter('target_path', array('eq' => 'catalog/category/view/id/' . $category->getId()));		
+                                ->addFieldToFilter('target_path', array('eq' => 'catalog/category/view/id/' . $category->getId()));
 
 		foreach ($uris as $uri) {
                         Mage::getModel('ezzoom/page')->getCollection()
@@ -111,11 +111,11 @@ class Ezapps_Zoom_Model_Observer extends Mage_Core_Model_Abstract
 
 		Mage::getModel('ezzoom/page')->getCollection()
                                 ->addFieldToFilter('store_id', array('eq' => $store_id))
-                                ->addFieldToFilter('uri', array('eq' => "/catalog/category/view/id/" . $category->getId()))->delete();		
+                                ->addFieldToFilter('uri', array('eq' => "/catalog/category/view/id/" . $category->getId()))->delete();
 
 		Mage::getModel('ezzoom/page')->getCollection()
                                 ->addFieldToFilter('store_id', array('eq' => $store_id))
-                                ->addFieldToFilter('uri', array('like' => "/catalog/category/view/id/" . $category->getId() . '/%'))->delete();		
+                                ->addFieldToFilter('uri', array('like' => "/catalog/category/view/id/" . $category->getId() . '/%'))->delete();
         }
 
     }
@@ -124,20 +124,20 @@ class Ezapps_Zoom_Model_Observer extends Mage_Core_Model_Abstract
 
 	$orig_page = $cms_page->getPage()->getOrigData();
 	$stores = $orig_page['store_id'];
+	$stores = array();
 	if (count($stores) == 1 && $stores[0] == 0) {
-		$stores = array();
 		foreach (Mage::getModel('core/store')->getCollection() as $store) {
 			$stores[] = $store->getId();
 		}
 	}
-	
+
 	foreach ($stores as $store_id) {
 
 		Mage::getModel('ezzoom/page')->getCollection()
                         ->addFieldToFilter('store_id', array('eq' => $store_id))
                         ->addFieldToFilter('uri', array('eq' => "/{$orig_page['identifier']}"))->delete();
-		
-		$id = Mage::getStoreConfig('web/default/cms_home_page', $store_id);		
+
+		$id = Mage::getStoreConfig('web/default/cms_home_page', $store_id);
 
 		if ($id == $orig_page['identifier']) {
 			 Mage::getModel('ezzoom/page')->getCollection()
@@ -154,12 +154,12 @@ class Ezapps_Zoom_Model_Observer extends Mage_Core_Model_Abstract
                                 ->addFieldToFilter('store_id', array('eq' => $store_id))
                                 ->addFieldToFilter('uri', array('eq' => "/" . $uri->getRequestPath()))->delete();
                 }
-	
+
                 Mage::getModel('ezzoom/page')->getCollection()
                                 ->addFieldToFilter('store_id', array('eq' => $store_id))
                                 ->addFieldToFilter('uri', array('eq' => 'cms/page/view/page_id/' . $orig_page['page_id']))->delete();
 
-			
+
 	}
 
     }
